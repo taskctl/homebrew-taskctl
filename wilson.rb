cb454c3d4b0587b831b11595714aeee4b6477706
Yevhen Terentiev <eugene@terentev.net>
2019-11-26 21:05:03 +0200

    Update wilson.rb

--- wilson.rb
+++ wilson.rb
@@ -1,9 +1,9 @@
 class Wilson < Formula
-  version "0.2.1"
+  version "0.2.2"
   desc "Wilson - routine tasks automation toolkit"
   homepage "https://github.com/trntv/wilson"
   url "https://github.com/trntv/wilson/releases/download/#{version}/wilson-darwin-amd64.tar.gz"
-  sha256 "030f827ba196382ab3f596fd87afb63b7dd6eac93ab95ceb787b20af5833cbec"
+  sha256 "61f93e84b001c695f53b762809ea1cb98bde1d14c52d2a1a01d806dfc17d39e5"
 
   def install
     bin.install "wilson_darwin_amd64"
