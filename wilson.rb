6c08cfe4a43cd8f9a10f170ec62f20117fb35b84
Denis Golubovskiy <bukashk0zzz@me.com>
2020-01-14 14:54:32 +0200

    Update wilson.rb

--- wilson.rb
+++ wilson.rb
@@ -1,12 +1,15 @@
 class Wilson < Formula
-  version "0.3.1"
-  desc "Wilson - routine tasks automation toolkit"
+  desc "Wilson - developer's routine tasks automation toolkit. Simple modern alternative to GNU Make 🧰"
   homepage "https://github.com/trntv/wilson"
   url "https://github.com/trntv/wilson/releases/download/#{version}/wilson-darwin-amd64.tar.gz"
   sha256 "5e428579afbc4005b3730d34f3166f8b27c738bade2e95c49ec465120c0c307a"
+  version "0.3.1"
 
   def install
-    bin.install "wilson_darwin_amd64"
+    bin.install "wilson_darwin_amd64" => "wilson"
+  end
+
+  test do
+    system "#{bin}/wilson", "--version"
   end
 end
-
