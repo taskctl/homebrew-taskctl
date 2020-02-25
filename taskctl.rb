6c66e574a4445a37b7f03fa5a40989380bde0d38
Yevhen Terentiev <eugene@terentev.net>
2020-02-25 08:35:03 +0200

    Update taskctl.rb

--- taskctl.rb
+++ taskctl.rb
@@ -1,9 +1,9 @@
 class Taskctl < Formula
-  version "0.5.1"
+  version "0.6.0"
   desc "taskctl - developer's routine tasks automation toolkit. Simple modern alternative to GNU Make 🧰"
   homepage "https://github.com/taskctl/taskctl"
   url "https://github.com/taskctl/taskctl/releases/download/#{version}/taskctl-darwin-amd64.tar.gz"
-  sha256 "19ff967b631ab8c80879dc5abb35c75a2e6a16f1f1cbb8fc67a999e507f790e0"
+  sha256 "97f379f3cea27457188294a941b1df980aa2b8af203a179c2fa42cc96334a7ed"
 
   def install
     bin.install "taskctl_darwin_amd64" => "taskctl"
