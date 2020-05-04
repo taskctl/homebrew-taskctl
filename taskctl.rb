57c4e682b13b887dc7e62237efb6e3d22543febd
Yevhen Terentiev <yevhen.terentiev@gmail.com>
2020-05-04 18:02:00 +0300

    Brew formula update for taskctl version 1.0.0-alpha-2

--- taskctl.rb
+++ taskctl.rb
@@ -2,20 +2,22 @@
 class Taskctl < Formula
   desc ""
   homepage "https://github.com/taskctl/taskctl"
-  version "0.6.4"
+  version "1.0.0-alpha-2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/taskctl/taskctl/releases/download/0.6.4/taskctl_0.6.4_darwin_amd64.tar.gz"
-    sha256 "5e85a9c0970923fcb9d9c1680573986367766db59b8905150384136cdb97b528"
+    url "https://github.com/taskctl/taskctl/releases/download/1.0.0-alpha-2/taskctl_1.0.0-alpha-2_darwin_amd64.tar.gz"
+    sha256 "33fb3b8c5d8a97e90e127ea59660ab2660ee0217f46cc0d70f16e91a49499f26"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/taskctl/taskctl/releases/download/0.6.4/taskctl_0.6.4_linux_amd64.tar.gz"
-      sha256 "039bbd13f493dfefe22b4b809beece3b26988d0e625ea133e3441042b8a51541"
+      url "https://github.com/taskctl/taskctl/releases/download/1.0.0-alpha-2/taskctl_1.0.0-alpha-2_linux_amd64.tar.gz"
+      sha256 "deab0cd0f5190adff4ed266fd4bd404450047af73cdb3eead7a2bc8f318a7149"
     end
   end
 
   def install
-    bin.install "taskctl"
+    bin.install "program"
+    bash_completion.install "autocomplete/bash_completion.bash"
+    zsh_completion.install "autocomplete/zsh_completion.zsh"
   end
 end
