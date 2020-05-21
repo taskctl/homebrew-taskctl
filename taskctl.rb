92b7b2ccc111a56dadfb3448cd05d06b3a945112
Yevhen Terentiev <yevhen.terentiev@gmail.com>
2020-05-21 14:17:04 +0300

    Brew formula update for taskctl version 1.0.0-rc3

--- taskctl.rb
+++ taskctl.rb
@@ -2,22 +2,22 @@
 class Taskctl < Formula
   desc ""
   homepage "https://github.com/taskctl/taskctl"
-  version "1.0.3"
+  version "1.0.0-rc3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/taskctl/taskctl/releases/download/v1.0.3/taskctl_1.0.3_darwin_amd64.tar.gz"
-    sha256 "bf391eb4b188a735ae48612b5bf604e98a6dc93873ddf3a039bebc1fe0a875d2"
+    url "https://github.com/taskctl/taskctl/releases/download/1.0.0-rc3/taskctl_1.0.0-rc3_darwin_amd64.tar.gz"
+    sha256 "607de35f35ebb6973267caad940a3a265c306ff7b52919e3c5093c44bc74d400"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/taskctl/taskctl/releases/download/v1.0.3/taskctl_1.0.3_linux_amd64.tar.gz"
-      sha256 "56bd67be16fee3267a17a4b7cd1219ff274b2873177f1c4f06d510f67b14e0f9"
+      url "https://github.com/taskctl/taskctl/releases/download/1.0.0-rc3/taskctl_1.0.0-rc3_linux_amd64.tar.gz"
+      sha256 "63dbbb6a7e6a27959836fa9b45a0213ac1c22215a914ecaff62e5980777f019f"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
       else
-        url "https://github.com/taskctl/taskctl/releases/download/v1.0.3/taskctl_1.0.3_linux_armv6.tar.gz"
-        sha256 "8565fff327a48cdb05324409fa02a98792692dce15b96be49d3320243af0cef5"
+        url "https://github.com/taskctl/taskctl/releases/download/1.0.0-rc3/taskctl_1.0.0-rc3_linux_armv6.tar.gz"
+        sha256 "7fb81ffb8cc4499fa25b1000eeddd5f5f5baee14843a51f16310a187d37abb0c"
       end
     end
   end
